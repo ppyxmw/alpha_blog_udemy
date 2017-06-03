1154b7d48797fde527bcbab3654f8f6a0b02139e
Michael Worthington <ppyxmw@gmail.com>
2017-06-03 12:27:45 +0000

    creates new template, contoller action and routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'welcome#home'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   
-  get 'about', to: 'welcome#about'
-
+  resources :articles
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
