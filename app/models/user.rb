8ab224d44018c3c83f6e5b1d91af9b73ddba7ed4
Michael Worthington <ppyxmw@gmail.com>
2017-06-26 20:13:58 +0000

    adds validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-    
+  validates :username, presense: true, 
+            uniqueness: {case_senstive: false}, 
+            length:  {minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presense: true, length:  {maximum: 105 },
+            uniqueness: {case_senstive: false},
+            format: { with: VALID_EMAIL_REGEX } 
 end
