e2198db64d5a031d6631d08fc16918151ea01b58
Michael Worthington <ppyxmw@gmail.com>
2017-06-27 16:00:10 +0000

    corrected validation typos in model, added article meta data in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   has_many :articles
   before_save { self.email = email.downcase }
-  validates :username, presense: true, 
+  validates :username, presence: true, 
             uniqueness: {case_senstive: false}, 
             length:  {minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presense: true, length:  {maximum: 105 },
+  validates :email, presence: true, length:  {maximum: 105 },
             uniqueness: {case_senstive: false},
             format: { with: VALID_EMAIL_REGEX } 
 end
