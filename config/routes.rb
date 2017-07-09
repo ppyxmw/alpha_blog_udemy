eafeb038d340241b9134291344bfa0f4f66c30ec
Michael Worthington <ppyxmw@gmail.com>
2017-07-09 04:38:07 +0000

    add current_user to create atricles, add authentication restrictions in views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,19 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
   root 'pages#home'
+  
   get 'about', to: 'pages#about'
   
   resources :articles
   
   get 'signup', to: 'users#new'
+  
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
