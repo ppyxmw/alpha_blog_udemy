59c4b3435d25139827c8f6027c61b4c6f5ad53c6
Michael Worthington <ppyxmw@gmail.com>
2017-07-13 14:01:02 +0000

    adds signup failure test due to incomplete info

--- test/integration/sign_up_user_test.rb
+++ test/integration/sign_up_user_test.rb
@@ -11,4 +11,16 @@ class SignUpUserTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_match "MikeyBoi's", response.body
   end
+  
+  test "invalid user submission results in failure" do
+    get signup_path
+    assert_template 'users/new'
+    assert_no_difference 'User.count'do
+      post users_path, user: {username: "", email: "email@email.com", password: "password"}
+    end
+    assert_template 'users/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
 end
